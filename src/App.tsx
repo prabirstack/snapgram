4b92daa59cdeb98aaea76e12b940e28b46eaceff
prabirstack <prabirsacademy@gmail.com>
2024-03-02 22:24:52 +0530

    Instagram clone-React, typescript

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from 'react-router-dom'
+import SigninForm from './_auth/forms/SigninForm'
+import SignupForm from './_auth/forms/SignupForm'
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout'
+import { Home } from './_root/pages'
+import './globals.css'
+
+export default function App() {
+  return (
+    <main className='flex h-screen'>
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path='/sign-in' element={<SigninForm />} />
+          <Route path='/sign-up' element={<SignupForm />} />
+        </Route>
+        {/* public routes */}
+
+        {/* private route */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  )
+}
---
+++ src/_root/RootLayout.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+const RootLayout = () => {
+  return <div>RootLayout</div>
+}
+export default RootLayout
